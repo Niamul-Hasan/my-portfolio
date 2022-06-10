edfad4d13302d837b06e792674f8d5c103cdb0b2
Niamul Hasan Zead <zead.ece2014@gmail.com>
2022-06-11 02:13:14 +0600

    project detail page is done

--- src/Pages/ProjectDetails/ProjectDetails.js
+++ src/Pages/ProjectDetails/ProjectDetails.js
@@ -3,14 +3,23 @@ import { useParams } from 'react-router-dom';
 import pic1 from '../../assets/pchunk/c1.PNG';
 import pic2 from '../../assets/pchunk/c2.PNG';
 import pic3 from '../../assets/pchunk/c3.PNG';
+import pic4 from '../../assets/hiking/c1.PNG';
+import pic5 from '../../assets/hiking/c2.PNG';
+import pic6 from '../../assets/hiking/c3.PNG';
+import pic7 from '../../assets/dentist/c1.PNG';
+import pic8 from '../../assets/dentist/c2.PNG';
+import pic9 from '../../assets/dentist/c3.PNG';
 import { FaReact } from 'react-icons/fa';
 import { FaNodeJs } from 'react-icons/fa';
 import { DiJavascript1 } from 'react-icons/di';
 import { SiExpress } from 'react-icons/si';
 import { FaStripeS } from 'react-icons/fa';
+import { FaBootstrap } from 'react-icons/fa';
 import { SiMongodb } from 'react-icons/si';
 import { SiTailwindcss } from 'react-icons/si';
 import { DiGit } from 'react-icons/di';
+import { SiFirebase } from 'react-icons/si';
+
 const ProjectDetails = () => {
     const { id } = useParams();
     const projectID = parseInt(id);
@@ -48,13 +57,59 @@ const ProjectDetails = () => {
         },
         {
             id: 2,
-            name: 'Tamanna Akter',
-            img: [pic1, pic2]
+            name: 'Hiking House',
+            features: <ul style={{ listStyleType: 'disc' }}>
+                <li>This project is based on react and node.js</li>
+                <li>JWT is implemented for the purpose of acknowledgement base data transfer form database to client side.</li>
+                <li>MongoDB is used to storage and managing data</li>
+                <li>Firebase hosting is used to deploy data</li>
+            </ul>,
+            technologies: <>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-blue-500' data-tip='React'>
+                    <FaReact /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-green-500' data-tip='Nodejs'>
+                    <FaNodeJs /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-warning' data-tip='javascript'>
+                    <DiJavascript1 /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip ' data-tip='Expressjs'>
+                    <SiExpress /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-lime-500' data-tip='mongodb'>
+                    <SiMongodb /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-purple-500' data-tip='Bootstrap'>
+                    <FaBootstrap /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-yellow-500' data-tip='Firebase'>
+                    <SiFirebase /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-red-500' data-tip='Git'>
+                    <DiGit /></button>
+
+            </>,
+            url: 'https://hiking-house.web.app/',
+            img: [pic4, pic5, pic6]
         },
         {
             id: 3,
             name: 'Ami R Tmi',
-            img: [pic1, pic3]
+            features: <ul style={{ listStyleType: 'disc' }}>
+                <li>This project is based on react</li>
+                <li>This is a siplme SPA project.</li>
+                <li>Firebase and react firebase hooks are implemented for login method</li>
+                <li>User can sign up with both email & password and google account. </li>
+            </ul>,
+            technologies: <>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-blue-500' data-tip='React'>
+                    <FaReact /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-warning' data-tip='javascript'>
+                    <DiJavascript1 /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-yellow-500' data-tip='Firebase'>
+                    <SiFirebase /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-purple-500' data-tip='Bootstrap'>
+                    <FaBootstrap /></button>
+                <button className='btn btn-gray-600 text-center text-5xl mx-2 tooltip text-red-500' data-tip='Git'>
+                    <DiGit /></button>
+
+            </>,
+            url: 'https://your-private-dentist.web.app/',
+            img: [pic7, pic8, pic9]
         }
     ];
 
@@ -76,9 +131,9 @@ const ProjectDetails = () => {
             </div>
 
             <div className=' mx-auto text-xl w-4/5 grid lg:grid-cols-2 sm:grid-cols-1 gap-8 my-12'>
-                <div className='text-teal-500 bg-base-100 shadow-2xl px-8 text-clip'>
+                <div className='text-zinc-800 bg-base-100 shadow-2xl px-8 text-clip'>
                     <h1 className='uppercase text-teal-600 font-semibold text-2xl'>Name: {project.name}</h1>
-                    <h1 className='uppercase text-tomato-700 font-semibold text-2xl'>Key Features</h1>
+                    <h1 className='uppercase text-emerald-700 font-semibold text-2xl'>Key Features</h1>
                     {project.features}
                 </div>
                 <div className='px-5'>
